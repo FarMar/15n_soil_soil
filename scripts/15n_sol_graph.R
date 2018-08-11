aa68de4e6f8264f22037366e8955fdfb43060be1
Mark Farrell <chewie@Marks-MacBook-Air.local>
2018-08-11 20:12:52 +0930

    added don_din fig

--- scripts/15n_sol_graph.R
+++ scripts/15n_sol_graph.R
@@ -580,7 +580,7 @@ ggplot(data = all, aes(days, doc, colour = trt, size = no3)) + # x, y, colour by
                                     colour = "black") # panel outline
   )
 
-## tdn doc
+## tdn don
 
 ggplot(data = all, aes(days, tdn, colour = trt, size = don)) + # x, y, colour by treatment
   scale_colour_manual(values=c("#CCFF00", # all the colours in hexadecimal code in order of "colour = x" argument above
@@ -613,6 +613,49 @@ ggplot(data = all, aes(days, tdn, colour = trt, size = don)) + # x, y, colour by
     #  title = "Nitrogen", # Graph title
     colour = "Treatment", # Legend Title
     size = "DON"
+  ) +
+  theme(
+    panel.grid = element_blank(), # blank grid
+    # axis.text.y = element_text(angle = 90), # rotates labels
+    axis.text=element_text(size=14), # axis text size
+    axis.title = element_text(color="black", face="bold", size=18), # axis label text size
+    panel.background = element_rect(fill = "white", # panel background
+                                    colour = "black") # panel outline
+  )
+
+## tdn don_din
+
+ggplot(data = all, aes(days, tdn, colour = trt, size = don_din)) + # x, y, colour by treatment
+  scale_colour_manual(values=c("#CCFF00", # all the colours in hexadecimal code in order of "colour = x" argument above
+                               "#99FF00",
+                               "#66FF00",
+                               "#00CC00",
+                               "#336600",
+                               "#FFCC33",
+                               "#FF9933",
+                               "#CC9933",
+                               "#996600",
+                               "#993300",
+                               "#333333",
+                               "#0099FF"
+  )) +
+  #geom_point(size = 6, alpha = 0.6) + # Plot as a point graph, size, transparency
+  geom_jitter(alpha = 0.7, width = 0.5) + # Plot as a jittered point graph
+  stat_smooth(method = "loess", se = FALSE, size = 1) + # Adds trend line only
+  # stat_smooth(method = "loess", # Adds ribbon for SE / CI and allows transparency to be adjusted
+  #              colour = "red", 
+  #            geom = "ribbon", 
+  #           alpha = 0.05, #sets ribbon transparency
+  #          size = 0.0001, #sets ribbon outline thickness. Need to work out how to make transparent
+  #         fullrange = TRUE) + # Forces ribbon to end of data
+  # xlim(0, 48) + ylim(0, 300) + # can be used to set hard limits to scale
+  coord_cartesian(xlim = c(0, 48), ylim = c(0, 520)) + #sets soft limits to scale
+  labs(
+    x = "Time (days)", # x axis label
+    y = TDN ~mg~L^{-1}, #y axis label, note cpde and weblink above for special characters and script
+    #  title = "Nitrogen", # Graph title
+    colour = "Treatment", # Legend Title
+    size = "DON:DIN"
   ) +
   theme(
     panel.grid = element_blank(), # blank grid
